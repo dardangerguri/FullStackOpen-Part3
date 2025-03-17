58a75d2c6f2531cc2e4d7ad7032da359634281b0
Dardan <dardangerguri@outlook.com>
2025-03-17 14:00:41 +0200

    ex3.11-Phonebook: Build and deploy frontend with backend
    - Added 'build:ui' script to generate the frontend production build and copy it to the backend directory.
    - Removed CORS configuration from the backend, as it's no longer necessary with the development proxy setup.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors');
 const app = express()
 
 morgan.token('body', (request, response) => {
   return request.method === 'POST' ? JSON.stringify(request.body) : ''
 })
 
-app.use(express.json())
+app.use(express.static('dist'))
+app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors());
 
 let persons = [
   {
