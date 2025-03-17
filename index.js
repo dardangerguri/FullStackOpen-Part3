e1cb57d5be5974ce353b6c89ebac47618ef6bd4c
Dardan <dardangerguri@outlook.com>
2025-03-17 11:43:27 +0200

    ex3.9-Phonebook: Enable CORS and update port configuration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors');
 const app = express()
 
 morgan.token('body', (request, response) => {
@@ -8,6 +9,7 @@ morgan.token('body', (request, response) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors());
 
 let persons = [
   {
@@ -93,7 +95,7 @@ app.get('/info', (request, response) => {
   `)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
