4a7fb11e187401ff7b75256828ca25ae21448305
Dardan <dardangerguri@outlook.com>
2025-03-22 20:00:35 +0200

    ex3.18-Phonebook: Update api/persons/:id and info routes to use database
    -Modified the backend to connect the /api/persons/:id and /info routes to the database.

--- index.js
+++ index.js
@@ -26,43 +26,21 @@ app.use(express.static('dist'))
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then(persons => response.json(persons))
     .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person)
-    response.json(person)
-  else
-    response.status(404).end('Person not found')
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person)
+        response.json(person)
+      else
+        response.status(404).end('Person not found')
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -108,11 +86,15 @@ app.post('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `)
+app.get('/info', (request, response, next) => {
+  Person.estimatedDocumentCount()
+    .then(count => {
+      response.send(`
+        <p>Phonebook has info for ${count} people</p>
+        <p>${new Date()}</p>
+      `)
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
