a4f0f3f9191e2bbe171a9d9ddd11d85ad02e5d32
Dardan <dardangerguri@outlook.com>
2025-03-22 19:51:26 +0200

    ex3.17-Phonebook: Handle updating existing phonebook entries with PUT request
    -Modified backend to support HTTP PUT request for updating existing phonebook entries.

--- index.js
+++ index.js
@@ -65,6 +65,19 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end('Person not found')
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(person => {
