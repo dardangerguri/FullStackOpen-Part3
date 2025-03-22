10b7ded5e1186f83e7ac930863aa6b42f43bb971
Dardan <dardangerguri@outlook.com>
2025-03-22 18:29:21 +0200

    ex3.16-Phonebook: Move error handling to dedicated middleware

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ morgan.token('body', (request, response) => {
   return request.method === 'POST' ? JSON.stringify(request.body) : ''
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(express.static('dist'))
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -35,8 +49,10 @@ let persons = [
   }
 ]
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => response.json(persons))
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => response.json(persons))
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -49,13 +65,18 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end('Person not found')
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(() => response.status(204).end())
-    .catch(error => response.status(404).end('Person not found'))
+    .then(person => {
+      if (person)
+        response.status(204).end()
+      else
+        response.status(404).end('Person not found')
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -71,7 +92,7 @@ app.post('/api/persons', (request, response) => {
 
   person.save()
     .then(savedPerson => response.json(savedPerson))
-    .catch(error => response.status(400).json({error: error.message}))
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -80,6 +101,9 @@ app.get('/info', (request, response) => {
     <p>${new Date()}</p>
   `)
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
